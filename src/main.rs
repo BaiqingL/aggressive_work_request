3b4b53e1a1823cfc5adbe814c462952d7eb5b463
BaiqingL <baiqinglyu@gmail.com>
2020-04-16 19:02:46 -0400

    Formatted code, thank the crabs for rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
+extern crate chrono;
 extern crate ini;
 extern crate libc;
 extern crate rev_lines;
-extern crate chrono;
 
+use chrono::Utc;
 use ini::Ini;
-use std::env;
-use std::process;
-use std::fs::File;
 use rev_lines::RevLines;
-use std::io::BufReader;
-use std::{thread, time};
 use std::collections::LinkedList;
-use chrono::{Utc};
+use std::env;
+use std::fs::File;
+use std::io::BufReader;
+use std::process;
+use std::{thread, time};
 
 fn main() {
-    unsafe{
+    unsafe {
         let _uid = libc::getuid();
 
-        if _uid == 0{
+        if _uid == 0 {
             println!("拥有root权限");
         } else {
             println!("没有root权限,可能会出现文件权限阅读问题");
@@ -26,37 +26,57 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     let conf = Ini::load_from_file(&args[1]).unwrap();
-    let control_location = conf.section(Some("settings")).unwrap().get("FAHLocation").unwrap();
-    let log_location = conf.section(Some("settings")).unwrap().get("LogLocation").unwrap();
+    let settings_sector = conf.section(Some("settings")).unwrap();
+    let control_location = settings_sector.get("FAHLocation").unwrap();
+    let log_location = settings_sector.get("LogLocation").unwrap();
+    let reload_limit: i32 = settings_sector
+        .get("ReloadThreshold")
+        .unwrap()
+        .parse()
+        .unwrap();
+    let idle_timeout: i64 = settings_sector.get("Idletimeout").unwrap().parse().unwrap();
+    let upload_timeout: i64 = settings_sector
+        .get("Uploadtimeout")
+        .unwrap()
+        .parse()
+        .unwrap();
+    let download_timeout: i64 = settings_sector
+        .get("Downloadtimeout")
+        .unwrap()
+        .parse()
+        .unwrap();
+    let sleep_interval: u64 = settings_sector
+        .get("SleepInterval")
+        .unwrap()
+        .parse()
+        .unwrap();
     let slots = conf.section(Some("slots")).unwrap();
-    let idle_timeout: i64 = conf.section(Some("settings")).unwrap().get("Idletimeout").unwrap().parse().unwrap();
-    let upload_timeout: i64 = conf.section(Some("settings")).unwrap().get("Uploadtimeout").unwrap().parse().unwrap();
-    let download_timeout: i64 = conf.section(Some("settings")).unwrap().get("Downloadtimeout").unwrap().parse().unwrap();
-    let reload_limit: i32 = conf.section(Some("settings")).unwrap().get("ReloadThreshold").unwrap().parse().unwrap();
-    let sleep_interval: u64 = conf.section(Some("settings")).unwrap().get("SleepInterval").unwrap().parse().unwrap();
-    println!("使用一下参数：\nFAHControl 脚本位置: {}\n日志位置: {}", control_location, log_location);
+    println!(
+        "使用一下参数：\nFAHControl 脚本位置: {}\n日志位置: {}",
+        control_location, log_location
+    );
     println!("开始检查");
 
     loop {
         let mut slotid: LinkedList<String> = LinkedList::new();
-        for prop in slots.iter(){
+        for prop in slots.iter() {
             slotid.push_back(String::from(prop.1));
         }
         let mut unworking_slots = 0;
         let slot_id_iter = slotid.iter();
-        for slot in slot_id_iter{
+        for slot in slot_id_iter {
             let state = find_slot_state(&slot, &log_location);
-            if state != 1{
-                if state == 3{
-                    if determine_download_exceed_limit(&download_timeout, &slot, &log_location){
+            if state != 1 {
+                if state == 3 {
+                    if determine_download_exceed_limit(&download_timeout, &slot, &log_location) {
                         unworking_slots += 1;
                     }
                 } else if state == 4 {
-                    if determine_idle_exceed_limit(&idle_timeout, &slot, &log_location){
+                    if determine_idle_exceed_limit(&idle_timeout, &slot, &log_location) {
                         unworking_slots += 1;
                     }
                 } else if state == 2 {
-                    if determine_upload_exceed_limit(&upload_timeout, &slot, &log_location){
+                    if determine_upload_exceed_limit(&upload_timeout, &slot, &log_location) {
                         unworking_slots += 1;
                     }
                 } else if state == 5 {
@@ -64,71 +84,115 @@ fn main() {
                 }
             }
             if unworking_slots == reload_limit {
-                process::Command::new(&control_location).arg("reload").output().ok().expect("出错!");
+                process::Command::new(&control_location)
+                    .arg("reload")
+                    .output()
+                    .ok()
+                    .expect("出错!");
                 println!("[提示] {} 重启", Utc::now());
-                unworking_slots = 0;
+                break;
             }
         }
-        thread::sleep(time::Duration::from_secs(60*sleep_interval));
+        thread::sleep(time::Duration::from_secs(60 * sleep_interval));
     }
-
 }
 
+/*
+状态
+1: 工作/其它
+2: 上传
+3: 下载
+4: 待机
+5: 不存在FSXX
+*/
+fn find_slot_state(slot: &str, log_location: &str) -> i32 {
+    let log = File::open(log_location).unwrap();
+    let rev_lines = RevLines::new(BufReader::new(&log)).unwrap();
 
-fn determine_download_exceed_limit(download_timeout: &i64, slot_id: &str, log_location: &str) -> bool{
-    let latest_action_time = find_state_gap("Downloading", "Download", log_location,slot_id, false);
-    if latest_action_time == 0{
+    for line in rev_lines {
+        if line.contains(slot) {
+            if determine_working_state(&line) {
+                return 1;
+            } else if line.contains("Uploading") || line.contains("Upload") {
+                return 2;
+            } else if line.contains("Downloading") || line.contains("Download") {
+                return 3;
+            } else if line.contains("WARNING") || line.contains("ERROR") {
+                return 4;
+            }
+            return 1;
+        }
+    }
+    5
+}
+
+fn determine_download_exceed_limit(
+    download_timeout: &i64,
+    slot_id: &str,
+    log_location: &str,
+) -> bool {
+    let latest_action_time =
+        find_last_action_time("Downloading", "Download", log_location, slot_id, false);
+    if latest_action_time == 0 {
         return false;
     }
     let now = Utc::now().timestamp() / 86400;
     let gap = now - latest_action_time;
     if gap.abs() > *download_timeout {
-        return true
+        return true;
     }
     false
 }
 
-fn determine_idle_exceed_limit(idle_timeout: &i64, slot_id: &str, log_location: &str) -> bool{
-    let latest_action_time = find_state_gap("WARNING", "ERROR", log_location,slot_id,  true);
-    if latest_action_time == 0{
+fn determine_idle_exceed_limit(idle_timeout: &i64, slot_id: &str, log_location: &str) -> bool {
+    let latest_action_time = find_last_action_time("WARNING", "ERROR", log_location, slot_id, true);
+    if latest_action_time == 0 {
         return false;
     }
     let now = Utc::now().timestamp() / 86400;
     let gap = now - latest_action_time;
     if gap.abs() > *idle_timeout {
-        return true
+        return true;
     }
     false
 }
 
-fn determine_upload_exceed_limit(upload_timeout: &i64, slot_id: &str, log_location: &str) -> bool{
-    let latest_action_time = find_state_gap("Uploading", "upload", log_location, slot_id, false);
-    if latest_action_time == 0{
+fn determine_upload_exceed_limit(upload_timeout: &i64, slot_id: &str, log_location: &str) -> bool {
+    let latest_action_time =
+        find_last_action_time("Uploading", "upload", log_location, slot_id, false);
+    if latest_action_time == 0 {
         return false;
     }
     let now = Utc::now().timestamp() / 86400;
     let gap = now - latest_action_time;
     if gap.abs() > *upload_timeout {
-        return true
+        return true;
     }
     false
 }
 
-fn find_state_gap(first_pattern: &str,second_pattern: &str, log_location: &str, slot_id: &str, idle_check: bool) -> i64{
+fn find_last_action_time(
+    first_pattern: &str,
+    second_pattern: &str,
+    log_location: &str,
+    slot_id: &str,
+    idle_check: bool,
+) -> i64 {
     let mut found_current_state = false;
 
     let log = File::open(log_location).unwrap();
     let rev_lines = RevLines::new(BufReader::new(&log)).unwrap();
 
     let mut latest_action_time: i64 = 0;
-    
+
     for line in rev_lines {
-        if line.contains(slot_id){
-            if line.contains(second_pattern) && !found_current_state 
-            || line.contains(first_pattern) && !found_current_state{
+        if line.contains(slot_id) {
+            if line.contains(second_pattern) && !found_current_state
+                || line.contains(first_pattern) && !found_current_state
+            {
                 found_current_state = true;
                 let unparsed_time;
-                if idle_check{
+                if idle_check {
                     unparsed_time = line.to_string()[6..14].replace(":", "");
                 } else {
                     unparsed_time = line.to_string()[..8].replace(":", "");
@@ -143,44 +207,16 @@ fn find_state_gap(first_pattern: &str,second_pattern: &str, log_location: &str, 
     latest_action_time
 }
 
-/*
-状态
-1: 工作/其它
-2: 上传
-3: 下载
-4: 待机
-5: 不存在FSXX
-*/
-fn find_slot_state(slot: &str, log_location: &str) -> i32 {
-    let log = File::open(log_location).unwrap();
-    let rev_lines = RevLines::new(BufReader::new(&log)).unwrap();
-    
-    for line in rev_lines {
-        if line.contains(slot){
-            if determine_working_state(&line){
-                return 1
-            } else if line.contains("Uploading") || line.contains("Upload"){
-                return 2
-            } else if line.contains("Downloading") || line.contains("Download"){
-                return 3
-            } else if line.contains("WARNING") || line.contains("ERROR"){
-                return 4
-            }
-            return 1
-        } 
-    }
-    5
-}
-
 fn determine_working_state(log: &str) -> bool {
-    if log.contains("Completed") 
-    || log.contains("Running FahCore") 
-    || log.contains("Starting") 
-    || log.contains("Received Unit:") 
-    || log.contains("Reading tar file") 
-    || log.contains("Digital signatures verified")
-    || log.contains("Temperature control disabled"){
-        return true
+    if log.contains("Completed")
+        || log.contains("Running FahCore")
+        || log.contains("Starting")
+        || log.contains("Received Unit:")
+        || log.contains("Reading tar file")
+        || log.contains("Digital signatures verified")
+        || log.contains("Temperature control disabled")
+    {
+        return true;
     }
     false
 }
